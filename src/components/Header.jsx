6eb4093697c719e70c72cf98cbbf1076c12845cb
Haris <82910362+yunekoh@users.noreply.github.com>
2023-12-18 00:26:00 +0100

    Change title and favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="header">
-      <h2 className="header--title">Meme Generator</h2>
+      <h2 className="header--title">React Meme Party App 🎉</h2>
     </header>
   );
 }
