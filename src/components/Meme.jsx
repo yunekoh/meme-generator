4087045e9cb72631c0573223f0d2e8948fe65d2b
Haris <82910362+yunekoh@users.noreply.github.com>
2023-12-18 00:34:47 +0100

    async function

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -8,10 +8,23 @@ export default function Meme() {
   });
   const [allMemes, setAllMemes] = React.useState([]);
 
+  // React.useEffect(() => {
+  //   fetch("https://api.imgflip.com/get_memes")
+  //     .then((res) => res.json())
+  //     .then((data) => setAllMemes(data.data.memes));
+  // }, []);
+
+  // ASYNC FUNC
   React.useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then((res) => res.json())
-      .then((data) => setAllMemes(data.data.memes));
+    async function getMemes() {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json();
+      setAllMemes(data.data.memes);
+    }
+    getMemes();
+
+    // CLEANUP
+    // return () => {};
   }, []);
 
   function getMemeImage() {
